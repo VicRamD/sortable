cdeb2c8d7814d658843bb8df42fe956c1cd28150
Víctor Francisco Ramírez Díaz <franvictor18@gmail.com>
2026-04-05 11:32:29 -0300

    multiples listas ordenables y de objetos intercambiables

--- src/components/drag-drop/SortableSection.jsx
+++ src/components/drag-drop/SortableSection.jsx
@@ -1,12 +1,18 @@
 import React, { useRef, useState } from 'react';
 import { DragDropProvider } from '@dnd-kit/react';
-import { useSortable } from '@dnd-kit/react/sortable';
+//import { useSortable } from '@dnd-kit/react/sortable';
 import { move } from '@dnd-kit/helpers';
 
 import Sortable from './Sortable';
+import SortableColumn from './SortableColumn';
 
 export default function SortableSection() {
-  const [items, setItems] = useState([0,1,2,3,4,5]);
+  //const [items, setItems] = useState([0,1,2,3,4,5]);
+  const [items, setItems] = useState({
+    A: ['A0', 'A1', 'A2'],
+    B: ['B0', 'B1'],
+    C: [],
+  });
 
   return (
     <DragDropProvider
@@ -14,11 +20,21 @@ export default function SortableSection() {
         setItems((items) => move(items, event));
       }}
     >
-      <ul className="list bg-amber-400">
+
+      <div className='inline-flex flex-row gap-5'>
+          {Object.entries(items).map(([column, items]) => (
+              <SortableColumn key={column} id={column}>
+                {items.map((id, index) => (
+                  <Sortable key={id} id={id} index={index} />
+                  ))}
+              </SortableColumn>
+          ))}
+      </div>
+      {/* <ul className="list bg-amber-400">
         {items.map((id, index) => (
           <Sortable key={id} id={id} index={index} />
         ))}
-      </ul>
+      </ul> */}
     </DragDropProvider>
   );
 }
